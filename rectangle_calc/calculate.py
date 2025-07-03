d8e37f15cd11fc2a0cb6dbc1f810770de287bed8
tsajoathato <thatotsajoa@icloud.com>
2025-07-03 03:00:53 +0200

    My Learning Python Project

---
+++ rectangle_calc/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #funtion to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    #function to calculate perimeter
+    return 2 * (length + width)
